36663781cce6d43294dc4491656203611bab4231
Mohammad Javad <mhosseini3458@gmail.com>
2021-09-19 13:08:08 +0430

    add morgan to services

--- infoApp/src/app.js
+++ infoApp/src/app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const axios = require('axios')
 const port = process.env.INFOPORT || 8083;
+const morgan = require('morgan')
 
+app.use(morgan('combined'))
 
 app.get('/', async (req, res) => {
     try {
-        const result = await axios('http://log-depl:8082/allLogs') //log-srv
+        const result = await axios('http://log-depl:8082/allLogs') //log-depl
         return res.send(result.data.logs)
     } catch (err) {
         return res.send(err)
